5689b4825697d26d81005875b8e6fa6364aed3ee
ummahabibahabo <habiba.enh@gmail.com>
2025-12-12 01:16:12 +0600

    Daily meals section data post and get commit

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 const port = 3000;
 
 // middleware
-
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kailwc7.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("local_chef_bazaar_db");
+
+    const DailyFoodsCollection = db.collection("dailymeals");
+    // Post for Daily meals Section
+    app.post("/dailymeals", async (req, res) => {
+      const newDailyMeals = req.body;
+      const result = await DailyFoodsCollection.insertOne(newDailyMeals);
+      res.send(result);
+    });
+
+    // Get for Daily meals Section
+    app.get("/dailymeals", async (req, res) => {
+      const cursor = DailyFoodsCollection.find().sort({ price: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Local chef bazar!");
